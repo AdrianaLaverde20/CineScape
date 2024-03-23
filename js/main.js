e1d9779631f21c2bb2010a5961c2f30406bd3cef
AdrianaLaverde20 <137790861+AdrianaLaverde20@users.noreply.github.com>
2024-03-23 11:11:46 -0500

    commit initial

---
+++ js/main.js
@@ -0,0 +1,31 @@
+function abrirPelicula(){
+    let pelicula = document.getElementById('modal-pelicula')
+    pelicula.classList.toggle('modal');
+}
+
+function abrirPelicula1(){
+    let pelicula = document.getElementById('modal-pelicula1')
+    pelicula.classList.toggle('modal');
+}
+function abrirPelicula2(){
+    let pelicula = document.getElementById('modal-pelicula2')
+    pelicula.classList.toggle('modal');
+}
+function abrirPelicula3(){
+    let pelicula = document.getElementById('modal-pelicula3')
+    pelicula.classList.toggle('modal');
+}
+function abrirPelicula4(){
+    let pelicula = document.getElementById('modal-pelicula4')
+    pelicula.classList.toggle('modal');
+}
+function abrirPelicula5(){
+    let pelicula = document.getElementById('modal-pelicula5')
+    pelicula.classList.toggle('modal');
+}
+function cerrarPanel(){
+    let panel = document.getElementById('panel-lateral');
+    panel.classList.toggle('active');
+}
+
+
